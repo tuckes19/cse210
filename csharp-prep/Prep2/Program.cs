a6affa099178bb00aa1f7ed9ea6a7c71c6553a6b
Seth Tucker <tuc19005@byui.edu>
2023-04-20 16:19:26 -0600

    Completed the Prep 2 Exercise

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userPercentageText = Console.ReadLine();
+        int userPercentage = int.Parse(userPercentageText);
+
+        string grade = "";
+        string word = " ";
+        string message = "";
+
+        if (userPercentage >= 90)
+        {
+            grade = "A";
+            word = "n ";
+            message = "Congratulations! You passed!";
+        }
+        else if (userPercentage < 90 && userPercentage >= 80)
+        {
+            grade = "B";
+            message = "Congratulations! You passed!";
+        }
+        else if (userPercentage < 80 && userPercentage >= 70)
+        {
+            grade = "C";
+            message = "Congratulations! You passed!";
+        }
+        else if (userPercentage < 70 && userPercentage >= 60)
+        {
+            grade = "D";
+            message = "You did not pass. Better luck next time";
+        }
+        else
+        {
+            grade = "F";
+            word = "n ";
+            message = "You did not pass. Better luck next time.";
+        }
+
+        Console.Write($"You received a{word}{grade}. {message}");
     }
 }
