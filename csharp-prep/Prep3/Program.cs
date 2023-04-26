e50a14ba4be405a45c67fa29b9c9d6647952a13a
Seth Tucker <tuc19005@byui.edu>
2023-04-26 15:28:05 -0600

    Completed Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        Console.WriteLine("This is the Magic Number Game!");
+        Console.Write("What is your guess? ");
+        string user_guess_text = Console.ReadLine();
+        int user_guess = int.Parse(user_guess_text);
+        
+        while (user_guess != number)
+        {
+            if (user_guess > number)
+            {
+                Console.WriteLine("Lower");
+                Console.Write("What is your guess? ");
+                user_guess_text = Console.ReadLine();
+                user_guess = int.Parse(user_guess_text);
+            }
+            else if (user_guess < number)
+            {
+                Console.WriteLine("Higher");
+                Console.Write("What is your guess? ");
+                user_guess_text = Console.ReadLine();
+                user_guess = int.Parse(user_guess_text);
+            }
+        }
+
+        Console.WriteLine("You guessed it!");
     }
 }
